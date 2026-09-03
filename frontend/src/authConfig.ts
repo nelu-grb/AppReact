053549d65864ec0db803d479178dee4e495ccfc7
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-09-03 09:40:05 -0400

    cambio a organizations (tenant id)

--- frontend/src/authConfig.ts
+++ frontend/src/authConfig.ts
@@ -4,7 +4,7 @@ import { LogLevel } from '@azure/msal-browser';
 export const msalConfig: Configuration = {
   auth: {
     clientId: import.meta.env.VITE_AZURE_CLIENT_ID,
-    authority: `https://login.microsoftonline.com/${import.meta.env.VITE_AZURE_TENANT_ID}`,
+    authority: `https://login.microsoftonline.com/${import.meta.env.VITE_AZURE_TENANT_ID || 'organizations'}`,
     redirectUri: import.meta.env.VITE_AZURE_REDIRECT_URI || window.location.origin,
     postLogoutRedirectUri: window.location.origin,
   },
