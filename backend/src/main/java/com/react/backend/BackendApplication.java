9a435efce6a4e64039c4d09c73c0063d1f7f2ae1
ZATO <sa.catalan@duocuc.cl>
2026-09-05 12:58:57 -0400

    ms-reservations funcional

--- backend/src/main/java/com/react/backend/BackendApplication.java
+++ backend/src/main/java/com/react/backend/BackendApplication.java
@@ -2,12 +2,14 @@ package com.react.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.react.backend", "com.andesstay.reservations"})
+@EnableJpaRepositories(basePackages = {"com.react.backend", "com.andesstay.reservations"})
+@EntityScan(basePackages = {"com.react.backend", "com.andesstay.reservations"})
 public class BackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
