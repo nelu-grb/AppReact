d80c91f7e9a8f26d7f3e71a38de28d2208709045
sophie <fifteenstepss@gmail.com>
2026-09-09 20:37:54 -0300

    bff

---
+++ backend/ms-bff/src/main/java/com/andesstay/msbff/MsBffApplication.java
@@ -0,0 +1,13 @@
+package com.andesstay.ms_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBffApplication.class, args);
+	}
+
+}
---
+++ backend/ms-bff/src/main/java/com/andesstay/msbff/dto/UnitType.java
@@ -0,0 +1,8 @@
+package com.andesstay.msbff.dto;
+
+public enum UnitType {
+    HABITACION,
+    SUITE,
+    APARTAMENTO,
+    CABANA
+}
