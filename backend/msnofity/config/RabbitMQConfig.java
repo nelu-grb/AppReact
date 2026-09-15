c42ca4496a3e692f70c6b79aca91a7c11277155b
sophie <fifteenstepss@gmail.com>
2026-09-14 23:15:29 -0300

    fix v2

--- backend/msnofity/config/RabbitMQConfig.java
+++ backend/msnofity/config/RabbitMQConfig.java
@@ -1,8 +1,8 @@
 package com.react.backend.msnofity.config;
 
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.support.DefaultJackson2JavaTypeMapper;
-import org.springframework.amqp.support.Jackson2JavaTypeMapper;
+import org.springframework.amqp.support.converter.DefaultJackson2JavaTypeMapper;
+import org.springframework.amqp.support.converter.Jackson2JavaTypeMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
